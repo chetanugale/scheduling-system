737f737d735ef7e874ce565834bad98091c5ecfe
chetan <44532446+chetanugale@users.noreply.github.com>
2025-05-15 12:55:49 +0530

    Update constants.go

--- constants/constants.go
+++ constants/constants.go
@@ -1,7 +1,7 @@
 package constants
 
 const(
-	MONGO_URI = "mongodb+srv://iamchetanugalemk31:WeLiN1mwy6cTMlN2@testcluster.nnsy78m.mongodb.net/"
+	MONGO_URI = ""
 	DB_NAME = "scheduler"
 	PORT = ":8080"
 	COLL_EVENTS="events"
